b1e9498bf16ebe2e2e34b6bd9323df75f525a681
Tushar Jana <tusharjana.aj3@gmail.com>
2026-06-09 12:32:12 +0530

    checking

--- src/app/form-signal/form-signal.ts
+++ src/app/form-signal/form-signal.ts
@@ -18,6 +18,7 @@ export class FormSignal {
     email:'',
     password:'',
   })
+  
 
   loginForm=form(this.loginModel,(schemaPath)=>{
     required(schemaPath.email,{message:'email is required'})
